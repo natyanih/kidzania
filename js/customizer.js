6321a932fa508135fa2e14b319c98af7008e243a
Nathanael O. Hinay <nathanael.hinay@globalzeal.net>
2015-03-29 19:14:49 +0800

    feature(customizer): add customizer buttons
    - add buttons for colors
    - add buttons for tint
    - add buttons for decal
    - add buttons for rims
    - add buttons for grill
    - add buttons for skirt
    - add buttons for spoiler

--- js/customizer.js
+++ js/customizer.js
@@ -3,7 +3,15 @@
 $( document ).ready( function ( $ ) {
 	var selectCar;
 	var currentCar = {
-		'name' : ''
+		'name'        : null,
+		'color'       : null,
+		'tint'        : null,
+		'decal'       : null,
+		'rim'         : null,
+		'grill'       : null,
+		'skirt'       : null,
+		'spoiler'     : null,
+		'currentSide' : null
 	};
 
 	function carousel () {
@@ -38,54 +46,54 @@ $( document ).ready( function ( $ ) {
 	}, 300);
 
 
-function hideDiv ( id ) {
+	function hideDiv ( id ) {
 
-	switch ( id ) {
-		// from carousel to customizer
-		case 'carousel' :
-			$( '#loading' ).hide();
-			$( '#carousel-main' ).hide();
-			$( '.customizer-main' ).show();
-			$( '.btn-view-car' ).show();
-			$( '.btn-customizer-group' ).show();
-			$( '.btn-select-car' ).hide();
+		switch ( id ) {
+			// from carousel to customizer
+			case 'carousel' :
+				$( '#loading' ).hide();
+				$( '#carousel-main' ).hide();
+				$( '.customizer-main' ).show();
+				$( '.btn-view-car' ).show();
+				$( '.btn-customizer-group' ).show();
+				$( '.btn-select-car' ).hide();
 
-			// hide other customizer divs
-			$( '.div-car-tint' ).hide();
-			$( '.div-car-decal' ).hide();
-			$( '.div-car-rims' ).hide();
-			$( '.div-car-body' ).hide();
+				// hide other customizer divs
+				$( '.div-car-tint' ).hide();
+				$( '.div-car-decal' ).hide();
+				$( '.div-car-rims' ).hide();
+				$( '.div-car-body' ).hide();
 
-			// add color to car color button
-			$( '.customizer-car-color > p' ).addClass( 'active' );
-			break;
+				// add color to car color button
+				$( '.customizer-car-color > p' ).addClass( 'active' );
+				break;
 
-		// customizer to car preview
-		case 'customizer' :
-			$( '#loading' ).hide();
-			$( '#carousel-main' ).hide();
-			$( '.customizer-main' ).hide();
-			$( '.preview-main' ).show();
-			$( '.btn-race-car' ).show();
-			$( '.btn-view-car' ).hide();
-			$( '.btn-customizer-group' ).hide();
-			break;
+			// customizer to car preview
+			case 'customizer' :
+				$( '#loading' ).hide();
+				$( '#carousel-main' ).hide();
+				$( '.customizer-main' ).hide();
+				$( '.preview-main' ).show();
+				$( '.btn-race-car' ).show();
+				$( '.btn-view-car' ).hide();
+				$( '.btn-customizer-group' ).hide();
+				break;
 
-		// car preview to race
-		case 'preview' :
-			$( '#loading' ).hide();
-			$( '#carousel-main' ).hide();
-			$( '#carousel' ).hide();
-			$( '.race-main' ).show();
-			$( '.race-main' ).removeClass( 'hidden' );
-			break;
+			// car preview to race
+			case 'preview' :
+				$( '#loading' ).hide();
+				$( '#carousel-main' ).hide();
+				$( '#carousel' ).hide();
+				$( '.race-main' ).show();
+				$( '.race-main' ).removeClass( 'hidden' );
+				break;
 
-		// if no id
-		default :
-			$( '#carousel' ).hide();
-			break;
+			// if no id
+			default :
+				$( '#carousel' ).hide();
+				break;
+			}
 		}
-	}
 
 	hideDiv();
 
@@ -96,6 +104,16 @@ function hideDiv ( id ) {
 		$( divClass ).show();
 		$( '.btn-customizer > p.active' ).removeClass( 'active' );
 		$( btnClass).find( 'p' ).addClass( 'active' );
+	}
+
+	function applyAsset( args ) {
+		var type = args.type;
+		var id   = args.id;
+
+		currentCar[ type ] = id;
+		console.log( currentCar );
+
+		console.log( 'type: ' + type + ' - id: ' + id );
 	}
 
 	// EVENT LISTENERS
@@ -133,6 +151,130 @@ function hideDiv ( id ) {
 		showCustomizer( 'body' );
 	} );
 
+	// COLORS EVENT HANDLERS
+	$( '.customizer-color-1' ).on( 'click', function () {
+		applyAsset( { 'type' : 'color', 'id' : 1 } );
+	} );
+
+	$( '.customizer-color-2' ).on( 'click', function () {
+		applyAsset( { 'type' : 'color', 'id' : 2 } );
+	} );
+
+	$( '.customizer-color-3' ).on( 'click', function () {
+		applyAsset( { 'type' : 'color', 'id' : 3 } );
+	} );
+
+	$( '.customizer-color-4' ).on( 'click', function () {
+		applyAsset( { 'type' : 'color', 'id' : 4 } );
+	} );
+
+	$( '.customizer-color-5' ).on( 'click', function () {
+		applyAsset( { 'type' : 'color', 'id' : 5 } );
+	} );
+
+	$( '.customizer-color-6' ).on( 'click', function () {
+		applyAsset( { 'type' : 'color', 'id' : 6 } );
+	} );
+
+	// TINT EVENT HANDLERS
+	$( '.customizer-tint-1' ).on( 'click', function () {
+		applyAsset( { 'type' : 'tint', 'id' : 1 } );
+	} );
+
+	$( '.customizer-tint-2' ).on( 'click', function () {
+		applyAsset( { 'type' : 'tint', 'id' : 2 } );
+	} );
+
+	// DECAL EVENT HANDLERS
+	$( '.customizer-decal-1' ).on( 'click', function () {
+		applyAsset( { 'type' : 'decal', 'id' : 1 } );
+	} );
+
+	$( '.customizer-decal-2' ).on( 'click', function () {
+		applyAsset( { 'type' : 'decal', 'id' : 2 } );
+	} );
+
+	$( '.customizer-decal-3' ).on( 'click', function () {
+		applyAsset( { 'type' : 'decal', 'id' : 3 } );
+	} );
+
+	$( '.customizer-decal-4' ).on( 'click', function () {
+		applyAsset( { 'type' : 'decal', 'id' : 4 } );
+	} );
+
+	$( '.customizer-decal-5' ).on( 'click', function () {
+		applyAsset( { 'type' : 'decal', 'id' : 5 } );
+	} );
+
+	$( '.customizer-decal-6' ).on( 'click', function () {
+		applyAsset( { 'type' : 'decal', 'id' : 6 } );
+	} );
+
+	// RIMS EVENT HANDLERS
+	$( '.customizer-rim-1' ).on( 'click', function () {
+		applyAsset( { 'type' : 'rim', 'id' : 1 } );
+	} );
+
+	$( '.customizer-rim-2' ).on( 'click', function () {
+		applyAsset( { 'type' : 'rim', 'id' : 2 } );
+	} );
+
+	$( '.customizer-rim-3' ).on( 'click', function () {
+		applyAsset( { 'type' : 'rim', 'id' : 3 } );
+	} );
+
+	$( '.customizer-rim-4' ).on( 'click', function () {
+		applyAsset( { 'type' : 'rim', 'id' : 4 } );
+	} );
+
+	$( '.customizer-rim-5' ).on( 'click', function () {
+		applyAsset( { 'type' : 'rim', 'id' : 5 } );
+	} );
+
+	$( '.customizer-rim-6' ).on( 'click', function () {
+		applyAsset( { 'type' : 'rim', 'id' : 6 } );
+	} );
+
+	// GRILL EVENT HANDLERS
+	$( '.customizer-grill-1' ).on( 'click', function () {
+		applyAsset( { 'type' : 'grill', 'id' : 1 } );
+	} );
+
+	$( '.customizer-grill-2' ).on( 'click', function () {
+		applyAsset( { 'type' : 'grill', 'id' : 2 } );
+	} );
+
+	$( '.customizer-grill-3' ).on( 'click', function () {
+		applyAsset( { 'type' : 'grill', 'id' : 3 } );
+	} );
+
+	// SKIRT EVENT HANDLERS
+	$( '.customizer-skirt-1' ).on( 'click', function () {
+		applyAsset( { 'type' : 'skirt', 'id' : 1 } );
+	} );
+
+	$( '.customizer-skirt-2' ).on( 'click', function () {
+		applyAsset( { 'type' : 'skirt', 'id' : 2 } );
+	} );
+
+	$( '.customizer-skirt-3' ).on( 'click', function () {
+		applyAsset( { 'type' : 'skirt', 'id' : 3 } );
+	} );
+
+	// SPOILER EVENT HANDLERS
+	$( '.customizer-spoiler-1' ).on( 'click', function () {
+		applyAsset( { 'type' : 'spoiler', 'id' : 1 } );
+	} );
+
+	$( '.customizer-spoiler-2' ).on( 'click', function () {
+		applyAsset( { 'type' : 'spoiler', 'id' : 2 } );
+	} );
+
+	$( '.customizer-spoiler-3' ).on( 'click', function () {
+		applyAsset( { 'type' : 'spoiler', 'id' : 3 } );
+	} );
+
+	// CONGRATS PAGE AND PRINTING
     window.addEventListener( 'DOMContentLoaded', function () {
 
         if( navigator.getUserMedia ) {
@@ -171,86 +313,5 @@ function hideDiv ( id ) {
     $( '.print-congrats' ).on( 'click', function () {
         window.print();
     } );
+
 } );
-
-
-
-// ( function () {
-// /* jshint -W098, -W117 */
-// 'use strict';
-
-// // properties of current car
-// var currentCar = {
-// 	'name'   : '',
-// 	'side'   : 'semi-left',
-// 	'color'  : '',
-// 	'decals' : '',
-// 	'tires'  : '',
-// 	'grills' : ''
-// };
-
-// var selectCar = 'car1';
-
-// // list of sides
-// //  semi-left = when car is displayed diagonally at the driver side
-// var sides = [ 'semi-left', 'left', 'back', 'right', 'semi-right', 'front' ];
-
-// // function to change image
-// // will use the currentCar properties
-// function changeImage () {
-// 	console.log( 'change image' );
-// }
-
-// function changeSide( rotation ) {
-// 	var currentIndex = sides.indexOf( currentCar.side );
-
-// 	switch( rotation ) {
-// 	case 'clockwise' :
-// 		// if current side is front, set it to semi-left
-// 		// else return the next items in the sides list
-// 		if ( currentIndex === 5 ) {
-// 			currentCar.side = sides[ 0 ];
-// 		} else {
-// 			currentCar.side = sides[ currentIndex    1 ];
-// 		}
-
-// 		changeImage();
-// 		break;
-
-// 	case 'counter' :
-
-// 		// if current side is front, set it to semi-left
-// 		// else return the next items in the sides list
-// 		if ( currentIndex === 0 ) {
-// 			currentCar.side = sides[ 5 ];
-// 		} else {
-// 			currentCar.side = sides[ currentIndex - 1 ];
-// 		}
-
-// 		changeImage();
-// 		break;
-// 	}
-// }
-
-// function carousel () {
-// 	$( '#carousel' ).carousel( { interval : 1500 } );
-// 	$( '#carousel' ).bind( 'slid.bs.carousel', function ( e ) {
-// 	   var carouselData = $( this ).data( 'bs.carousel' );
-
-// 		selectCar = carouselData.$element.find( '.item.active' ).attr( 'id' )
-// 	} );
-
-// 	$( '.car-select' ).on( 'click', function () {
-// 		$( '#customizer-div' ).removeClass( 'hidden' );
-// 		$( '#main-div' ) .addClass( 'hidden' );
-// 		$( '#carName' ).text( selectCar );
-// 		currentCar.name = selectCar;
-// 		console.log( currentCar )
-// 	} );
-
-// }
-
-
-// carousel();
-
-// } )();
